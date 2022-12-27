f3843c96c67ac125f548acccf00aac2282696b1c
Maxim <i0979887766@gmail.com>
2022-12-27 15:56:01 +0200

    Test commit 1.01

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoin
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyChariginig')
+    print_hi('PyCinig')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
