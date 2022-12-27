10435fd5cffec802bbfd469f9e11e42774bd059f
Maxim <i0979887766@gmail.com>
2022-12-27 15:54:05 +0200

    Test commit 1

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+print ('hvbfubgubguobuodbg')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoin
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyChariginiugiuhg')
