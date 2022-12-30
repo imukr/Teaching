ef2d7440eed6c04a6c693c1d507ef4a59fcba665
Imukr <i0979887766@gmail.com>
2022-12-30 15:48:52 +0200

    Commit 1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='useful',
+    version='1',
+    description='Very useful code',
+    url='http://github.com/dummy_user/useful',
+    author='Flying Circus',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=find_namespace_packages()
+)
